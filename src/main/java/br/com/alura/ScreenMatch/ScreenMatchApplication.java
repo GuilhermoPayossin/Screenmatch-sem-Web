fac171b84d9f125ad1c4df3824a39208067734b0
Guilhermo <guilhermo.payossin@gmail.com>
2025-03-04 09:27:05 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.ScreenMatch;
+
+import br.com.alura.ScreenMatch.model.DadosSerie;
+import br.com.alura.ScreenMatch.services.ConsumoApi;
+import br.com.alura.ScreenMatch.services.ConverterDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=invincible&apikey=689d4005");
+		System.out.println(json);
+		ConverterDados conversor = new ConverterDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
